cd51fef3ffb557ff4cecbea0741837a5e2436494
kayigireerneste <kayigireerneste@gmail.com>
2024-08-06 23:25:09 +0200

    ft/added user

--- Blogging-sytem-main/src/controllers/user.controller.js
+++ Blogging-sytem-main/src/controllers/user.controller.js
@@ -1,28 +1,29 @@
 import User from "../models/user.model";
 import { generateToken } from "../utils/token.generator";
-import { hashPassword,comparePassword } from "../utils/password.util";
+import { hashPassword, comparePassword } from "../utils/password.util";
 
 export const createUser = async (req, res) => {
     try {
-        const { names,  password, email } = req.body;
+        const { names, password, email } = req.body;
         const hashedPassword = await hashPassword(password);
         const existingEmail = await User.findOne({ where: { email } });
         if (existingEmail) {
             return res.status(409).json({ message: "The user already exists" });
         }
-    
+
         const user = await User.create({ names, password: hashedPassword, email });
         
         res.status(201).json({ 
-            message:"User created successfully, you can login now"
-             });
+            message: "User created successfully, you can login now"
+        });
     } catch (error) {
         res.status(500).json({ 
-            message:"failed to create user",
+            message: "Failed to create user",
             error: error.message
-         });
+        });
     }
 }
+
 export const userLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -34,15 +35,20 @@ export const userLogin = async (req, res) => {
         if (!isMatch) {
             return res.status(401).json({ message: "Invalid email or password" });
         }
-        const username = user.names;
         const token = generateToken(user);
         res.status(201).json({ 
-            message:"User logged in successfully",
-            data:{ token, username}});
+            message: "User logged in successfully",
+            data: {
+                token,
+                user: {
+                    name: user.names
+                }
+            }
+        });
     } catch (error) {
         res.status(500).json({ 
-            message: "internal server error",
+            message: "Internal server error",
             error: error.message
-         });
+        });
     }
 }
