ee28ae2297b99ee1d3abba9bf94935702e01f58a
kayigireerneste <kayigireerneste@gmail.com>
2024-08-06 11:11:53 +0200

    fx/signUP

--- Blogging-sytem-main/server.js
+++ Blogging-sytem-main/server.js
@@ -5,12 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
 connectToDatabase()
   .then(() => {
     console.log('Connected to the database');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error('Failed to connect to the database:', err);
--- Blogging-sytem-main/src/app.js
+++ Blogging-sytem-main/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from "./routes/user.routes";
 import blogRoutes from "./routes/blog.routes";
 import bodyParser from "body-parser";
@@ -7,20 +8,28 @@ import Blog from './models/blog.model.js';
 import Comment from './models/comments.model.js';
 import associateModels from './models/associateModels.js';
 
-
 const app = express();
 const models = { User, Blog, Comment };
 associateModels(models);
 
+// CORS configuration
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow your frontend's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-return res.status(200).json({message:"welcome to  QT APIs"})
-})
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: "Welcome to QT APIs" });
+});
+
 app.use('/api/users', userRoutes);
 app.use('/api/blogs', blogRoutes);
-
 
 export default app;
